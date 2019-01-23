19b3e5e5c5d20dcd6d84b2953fa7eff44cbc1411
mafeiben <mafeiben@u-road.com>
2019-01-23 18:03:47 +0800

    修复bug

--- app/src/main/java/com/uroad/dubai/common/DubaiConfig.kt
+++ app/src/main/java/com/uroad/dubai/common/DubaiConfig.kt
@@ -2,7 +2,7 @@ package com.uroad.dubai.common
 
 class DubaiConfig {
     companion object {
-        var isDebug = true   //debug模式 异常不上传到bugly
+        var isDebug = false   //debug模式 异常不上传到bugly
         var isApiDebug = true   //是否是正式环境开关
     }
 }
